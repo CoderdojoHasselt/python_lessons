c58ceb86ae90d96e4d29915617a08abd07858ec7
JonnyDaenen <jonnydaenen@MBP-1610001.local>
2017-09-23 17:39:13 +0200

    Comment tests

--- 02_printing_with_unit_test/lesson_tests.py
+++ 02_printing_with_unit_test/lesson_tests.py
@@ -1,35 +1,35 @@
-import os
-import unittest
-import subprocess
-import builtins
-import io
-import sys
-
-def fake_input(prompt):
-    # print(prompt)
-    return "jonny"
-
-class Capturing(list):
-    def __enter__(self):
-        self._stdout = sys.stdout
-        sys.stdout = self._stringio = io.StringIO()
-        self._input = builtins.input
-        builtins.input = fake_input
-        return self
-    def __exit__(self, *args):
-        self.extend(self._stringio.getvalue().splitlines())
-        del self._stringio    # free up some memory
-        sys.stdout = self._stdout
-        builtins.input = self._input
-
-class AdditionTests(unittest.TestCase):
-
-    def test_main(self):
-        with Capturing() as output:
-            import main
-        print(output)
-        self.assertEqual(output, ['Hello jonny!'])
-
-
-if __name__ == '__main__':
-    unittest.main()
+# import os
+# import unittest
+# import subprocess
+# import builtins
+# import io
+# import sys
+#
+# def fake_input(prompt):
+#     # print(prompt)
+#     return "jonny"
+#
+# class Capturing(list):
+#     def __enter__(self):
+#         self._stdout = sys.stdout
+#         sys.stdout = self._stringio = io.StringIO()
+#         self._input = builtins.input
+#         builtins.input = fake_input
+#         return self
+#     def __exit__(self, *args):
+#         self.extend(self._stringio.getvalue().splitlines())
+#         del self._stringio    # free up some memory
+#         sys.stdout = self._stdout
+#         builtins.input = self._input
+#
+# class AdditionTests(unittest.TestCase):
+#
+#     def test_main(self):
+#         with Capturing() as output:
+#             import main
+#         print(output)
+#         self.assertEqual(output, ['Hello jonny!'])
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
--- 02_printing_with_unit_test/testcapture.py
+++ 02_printing_with_unit_test/testcapture.py
@@ -1,36 +1,36 @@
-from io import StringIO
-import sys
-
-import builtins
-
-
-def fake_input(prompt):
-    print(prompt)
-    return "jonny"
-
-
-
-
-
-class Capturing(list):
-    def __enter__(self):
-        self._stdout = sys.stdout
-        sys.stdout = self._stringio = StringIO()
-        self._input = builtins.input
-        builtins.input = fake_input
-        return self
-    def __exit__(self, *args):
-        self.extend(self._stringio.getvalue().splitlines())
-        del self._stringio    # free up some memory
-        sys.stdout = self._stdout
-        builtins.input = self._input
-
-def f():
-    print("jonny")
-
-with Capturing() as output:
-    f()
-    import main
-
-print("output")
-print(output)
+# from io import StringIO
+# import sys
+#
+# import builtins
+#
+# 
+# def fake_input(prompt):
+#     print(prompt)
+#     return "jonny"
+#
+#
+#
+#
+#
+# class Capturing(list):
+#     def __enter__(self):
+#         self._stdout = sys.stdout
+#         sys.stdout = self._stringio = StringIO()
+#         self._input = builtins.input
+#         builtins.input = fake_input
+#         return self
+#     def __exit__(self, *args):
+#         self.extend(self._stringio.getvalue().splitlines())
+#         del self._stringio    # free up some memory
+#         sys.stdout = self._stdout
+#         builtins.input = self._input
+#
+# def f():
+#     print("jonny")
+#
+# with Capturing() as output:
+#     f()
+#     import main
+#
+# print("output")
+# print(output)
